b3581cf6681e2d112a63b79908eff0e04a231f93
fran <ffores93@gmail.com>
2024-05-21 13:07:16 +0200

    make level 2 fix errro two players

--- src/scenes/scene_levels/level_2/level_spleetscreen.gd
+++ src/scenes/scene_levels/level_2/level_spleetscreen.gd
@@ -2,7 +2,7 @@ extends Level_333
 
 
 func _ready() -> void:
-	var player_2 := %Player2 as Player
+	var player_2 := %Player2 as PlayerKnight
 	var viewport_1 := %Viewport1 as SubViewport
 	var viewport_2 := %Viewport2 as SubViewport
 	viewport_2.world_2d = viewport_1.world_2d
